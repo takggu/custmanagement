2d9072c540f34ae024a7b34c45febc3773705507
takggu <tkh99@naver.com>
2020-02-02 22:45:48 +0900

    Add Get Cust

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id':1,
-  'image':'https://placeimg.com/64/64/1',
-  'name': '탁규환',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id':2,
-  'image':'https://placeimg.com/64/64/2',
-  'name': '탁구공',
-  'birthday':'860829',
-  'gender':'남자',
-  'job':'프로그램'
-},
-{
-  'id':3,
-  'image':'https://placeimg.com/64/64/3',
-  'name': '김탁구',
-  'birthday':'890511',
-  'gender':'남자',
-  'job':'가정부'
-}
-]
-
 class App extends Component{
+
+  state = {
+      customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -63,7 +54,9 @@ class App extends Component{
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); })}
+              {this.state.customers ? this.state.customers.map(c => { 
+                return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); 
+              }) : ""}
             </TableBody>
           </Table>
         </Paper>
--- server.js
+++ server.js
@@ -10,4 +10,33 @@ app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!!!!!!!!!!!!'});
 });
 
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id':1,
+          'image':'https://placeimg.com/64/64/1',
+          'name': '탁규환',
+          'birthday':'961222',
+          'gender':'남자',
+          'job':'대학생'
+        },
+        {
+          'id':2,
+          'image':'https://placeimg.com/64/64/2',
+          'name': '탁구공',
+          'birthday':'860829',
+          'gender':'남자',
+          'job':'프로그램'
+        },
+        {
+          'id':3,
+          'image':'https://placeimg.com/64/64/3',
+          'name': '김탁구',
+          'birthday':'890511',
+          'gender':'남자',
+          'job':'가정부'
+        }
+    ]);
+});
+
 app.listen(port, () => console.log(`Listening on port t ${port}`));
