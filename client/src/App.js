d12f0ac5a9b5adda14dbc52fc87290885b900547
takggu <tkh99@naver.com>
2020-02-04 23:04:52 +0900

    loading

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,16 +18,40 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
+/*
+1) constructor()
 
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount()
+
+*/
+/*
+
+props or state => shouldComponentUpdate()
+
+이후 render() 호출
+
+*/
 class App extends Component{
 
   state = {
-      customers: ""
+      customers: "",
+      completed: 0
   }
 
+
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+
+    //데이터를 받아오는 부분 
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -36,6 +61,11 @@ class App extends Component{
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState( { completed: completed >= 100 ? 0 : completed + 1 });
   }
 
   render() {
@@ -56,7 +86,13 @@ class App extends Component{
             <TableBody>
               {this.state.customers ? this.state.customers.map(c => { 
                 return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); 
-              }) : ""}
+              }) : 
+              <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+              </TableRow>
+              }
             </TableBody>
           </Table>
         </Paper>
